55c54db3d4be261eeeab38fe6033bb071ac2e7d3
DevSlowz <msvieira021@gmail.com>
2025-03-08 17:44:41 -0500

    add 2 book entries and cli args

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import sys
 from stats import get_num_words, get_character_count,demo,print_results
 
 
 def get_book_text(path_to_file):
+
+
     with open(path_to_file) as f:
         file_contents = f.read()
 
     return file_contents
 
 
+def input_validation():
+    result = sys.argv
+    if len(result) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    return get_book_text(result[1])
 
 
 
@@ -15,7 +25,7 @@ def get_book_text(path_to_file):
 def main():
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
-    result = get_book_text("./books/frankenstein.txt")
+    result = input_validation()
     num_words = get_num_words(result)
 
     print("----------- Word Count ----------")

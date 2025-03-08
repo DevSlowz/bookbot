680b8c40270f2425d8a26c39efbd8580a12f1f94
DevSlowz <msvieira021@gmail.com>
2025-03-08 13:51:09 -0500

    Add function to track word count

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def get_book_text(path_to_file):
 
     return file_contents
 
+def string_to_total_words(book_content):
+    word_count = book_content.split()
+    return len(word_count)
 
 def main():
     result = get_book_text("./books/frankenstein.txt")
-    print(result)
+    num_words = string_to_total_words(result)
+    print(f"{num_words} words found in the document")
 
 
 main()
